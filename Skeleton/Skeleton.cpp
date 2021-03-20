66aeea1f893d01d13921d040deae99c39556dc63
HLCaptain <pkblazsak@gmail.com>
2021-03-21 00:40:31 +0100

    working status, bugs present

--- Skeleton/Skeleton.cpp
+++ Skeleton/Skeleton.cpp
@@ -58,6 +58,8 @@ const char * const fragmentSource = R"(
 	uniform sampler2D textureUnit;
 	uniform int isGPUProcedural;
 	uniform vec3 color;		// uniform variable, the color of the primitive
+	uniform vec3 randomColor1;
+	uniform vec3 randomColor2;
 
 	in vec2 texCoord;			// variable input: interpolated texture coordinates
 	
@@ -72,7 +74,7 @@ const char * const fragmentSource = R"(
 		return 0;
 	}
 
-	vec3 ProceduralColor(vec2 c) {
+	vec3 ProceduralCircleColor(vec2 c) {
 		float u = 0.5;
 		float v = 0.0;
 		float x = c.x - u;
@@ -80,15 +82,22 @@ const char * const fragmentSource = R"(
 		float y = c.y - v;
 		y = y * y;
 		if (x + y < 0.1) {
-			return vec3(1, 1, 0);
-		} else return vec3(0, 1, 1);
+			return randomColor1;
+		} else return randomColor2;
+	}
+
+	vec3 ProceduralSquareColor(vec2 c) {
+		if (c.x > 0.25 && c.x < 0.75 && c.y > -0.25 && c.y < 0.25) {
+			return randomColor1;
+		} else return randomColor2;
 	}
 
 	void main() {
 		if (isGPUProcedural != 0) {
-			int i = Mandelbrot(texCoord * 3 - vec2(2, 0)); 
-			outColor = vec4((i % 5)/5.0f, (i % 11) / 11.0f, (i % 31) / 31.0f, 1); 
-			outColor = vec4(ProceduralColor(texCoord), 1);
+			//int i = Mandelbrot(texCoord * 3 - vec2(2, 0)); 
+			//outColor = vec4((i % 5)/5.0f, (i % 11) / 11.0f, (i % 31) / 31.0f, 1); 
+			//outColor = vec4(ProceduralCircleColor(texCoord), 1);
+			outColor = vec4(ProceduralSquareColor(texCoord), 1);
 		} else {
 			outColor = vec4(color, 1);	// computed color is the color of the primitive
 		}
@@ -112,6 +121,7 @@ class HyperPoint {
 public:
 	vec2 position; // real position
 	vec2 bkproj; // projected onto the beltrami-klein disc
+	std::vector<HyperPoint*> pairs;
 
 	// vec2 is enough because w can be calculated with x and y
 	HyperPoint(const vec2 position = vec2(0, 0)) : position(position) { bkproj = getBKProj(); }
@@ -134,12 +144,16 @@ float hyperDistance(const HyperPoint& p, const HyperPoint& q) {
 }
 
 // Get the hyperbolic projection on a point, which is on the Beltrami-Klein disc
-vec3 getHyperProjFromBK(const vec3& p) { return p / (sqrtf(1 - p.x * p.x - p.y * p.y)); }
+vec3 getHyperProjFromBK(const vec3& p) {
+	if (1 - p.x * p.x - p.y * p.y > 0.01f) {
+		return p / (sqrtf(1 - p.x * p.x - p.y * p.y));
+	} else return p / sqrtf(0.01f);
+}
 
 // Calculating the direction vector from one point to another
 vec3 hyperDirectionVector(const HyperPoint& p, const HyperPoint& q) {
 	float distance = hyperDistance(p, q);
-	return (q.get3dPos() - p.get3dPos() * coshf(distance)) / sinhf(distance);
+	return (q.get3dPos() - p.get3dPos() * coshf(distance)) / fmaxf(sinhf(distance), .001f);
 }
 
 // Offseting a p point in v direction with a distance.
@@ -154,6 +168,11 @@ void hyperOffset(HyperPoint& p, const vec3& v, const float distance) {
 void pan(const HyperPoint& p, const HyperPoint& q, HyperPoint& point) {
 	// Don't need to pan when the two points are the same.
 	if (p.position.x == q.position.x && p.position.y == q.position.y) { return; }
+	if (length(p.bkproj) > length(getHyperProjFromBK(p.bkproj)) ||
+		length(q.bkproj) > length(getHyperProjFromBK(q.bkproj)) ||
+		length(point.bkproj) > length(getHyperProjFromBK(point.bkproj))) {
+		return;
+	}
 
 	// Mirrored point is temporary
 	HyperPoint tmp;
@@ -172,7 +191,9 @@ void pan(const HyperPoint& p, const HyperPoint& q, HyperPoint& point) {
 	hyperOffset(point, dirVec, distance / 2); // Half the distance
 }
 
-float circleDefaultRadius = 0.2f;
+// Default radius of the circle on the hyperbolic plane.
+const float circleDefaultRadius = 0.1f;
+
 // HyperCircle is a circle on the hyperbolic plane.
 class HyperCircle {
 public:
@@ -183,6 +204,8 @@ public:
 	float radius; // radius
 	unsigned int vao, vbo[2];
 	std::vector<vec2> vertexUVs;
+	vec3 outerColor; // outer color of the procedural texture
+	vec3 innerColor; // inner color of the procedural texture
 
 	HyperCircle() :
 		center(HyperPoint(vec2(0, 0))), // ini center as the 0,0,1
@@ -210,6 +233,10 @@ public:
 	}
 
 	void iniCircle(int numberOfVertices, const vec2& pos = vec2(0, 0)) {
+		center.updatePos(vec2(0, 0));
+		hyperVertices.clear();
+		vertexUVs.clear();
+		bkproj.clear();
 		// tesselate circumference
 		for (int i = 0; i < numberOfVertices; i++) {
 			float rad = M_PI * 2 * (float) i / (float) numberOfVertices;
@@ -234,6 +261,7 @@ public:
 
 	// updating the buffer
 	void updateBuf() {
+		glBindVertexArray(vao);
 		glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
 		glBufferData(GL_ARRAY_BUFFER, 	// Copy to GPU target
 			sizeof(vec2) * bkproj.size(),  // # bytes
@@ -248,6 +276,9 @@ public:
 
 	// Pan every point based on two points
 	void panCircle(const HyperPoint& p, const HyperPoint& q) {
+		if (p.position.x == q.position.x && p.position.y == q.position.y) {
+			return;
+		}
 		pan(p, q, center); // offseting center
 		// offsetting the points on the circumference
 		for (int i = 0; i < hyperVertices.size(); i++) {
@@ -284,12 +315,28 @@ public:
 		glVertexAttribPointer(1,       // vbo -> AttribArray 0
 			2, GL_FLOAT, GL_FALSE, // two floats/attrib, not fixed-point
 			0, NULL); 		     // stride, offset: tightly packed
+
+		iniColor();
+	}
+
+	void iniColor() {
+		innerColor = vec3(
+			(float)rand() / (float)RAND_MAX,
+			(float)rand() / (float)RAND_MAX,
+			(float)rand() / (float)RAND_MAX);
+		outerColor = vec3(
+			(float)rand() / (float)RAND_MAX,
+			(float)rand() / (float)RAND_MAX,
+			(float)rand() / (float)RAND_MAX);
 	}
 
 	void draw() {
 		// Activate
+		updateBuf();
 		glBindVertexArray(vao);
 
+		gpuProgram.setUniform(innerColor, "randomColor1"); // procedural texture
+		gpuProgram.setUniform(outerColor, "randomColor2"); // procedural texture
 		gpuProgram.setUniform(1, "isGPUProcedural"); // procedural texture
 		gpuProgram.setUniform(vec3(1.0f, 1.0f, 0.0f), "color"); // Yellow
 		glDrawArrays(GL_TRIANGLE_FAN, 0, bkproj.size()); // Drawing out all vertices, which is the bkproj array
@@ -309,6 +356,8 @@ public:
 	Edge(HyperPoint* start, HyperPoint* end) : ends(std::vector<HyperPoint*>()), vao(0), vbo(0) {
 		ends.push_back(start);
 		ends.push_back(end);
+		start->pairs.push_back(end);
+		end->pairs.push_back(start);
 	}
 
 	void create() {
@@ -359,14 +408,23 @@ public:
 	}
 };
 
-const int verticalInterval = 4;
-const int horizontalInterval = 4;
+const float verticalInterval = 24.0f;
+const float horizontalInterval = 24.0f;
+const float acceleration = 12.5f; // acceleration in unit/sec
+const float phi = 12.0f; // percentage in velocity lost in every second
+const float distance = 0.5f; // desired distance between vertices
 
 class Graph {
 public:
-	std::vector<Edge> edges;
-	std::vector<HyperCircle> vertices;
-	Graph(int numberOfVertices, float edgeChance) : edges(std::vector<Edge>()), vertices(std::vector<HyperCircle>()) {
+	std::vector<Edge> edges; // edges between vertices
+	std::vector<HyperCircle> vertices; // vertices of the graph
+	std::vector<vec2> velocity; // velocity for every vertex
+	const float accel = acceleration; // reversed acceleration, which lowers velocity
+	const float desiredDistance = distance; // desired distance between vertices
+	Graph(int numberOfVertices, float edgeChance) : 
+		edges(std::vector<Edge>()), 
+		vertices(std::vector<HyperCircle>()),
+		velocity(std::vector<vec2>()) {
 		// Make the graph.
 		iniVertices(numberOfVertices);
 		iniEdges(edgeChance);
@@ -377,9 +435,10 @@ public:
 		srand(420);
 		for (int i = 0; i < numberOfVertices; i++) {
 			float x, y;
-			x = (float) rand() / RAND_MAX * verticalInterval * 2 - verticalInterval;
-			y = (float) rand() / RAND_MAX * horizontalInterval * 2 - horizontalInterval;
-			vertices.push_back(HyperCircle(vec2(x, y)));
+			x = (float)rand() / RAND_MAX * verticalInterval * 2 - verticalInterval;
+			y = (float)rand() / RAND_MAX * horizontalInterval * 2 - horizontalInterval;
+			vertices.push_back(HyperCircle());
+			velocity.push_back(vec2(x, y));
 		}
 	}
 	// Inicializing all edges of the graph between vertices
@@ -387,7 +446,7 @@ public:
 		srand(69);
 		for (int i = 0; i < vertices.size(); i++) {
 			for (int j = i + 1; j < vertices.size(); j++) {
-				if ((float) rand() / (float) RAND_MAX < edgeChance) {
+				if ((float)rand() / RAND_MAX < edgeChance) {
 					edges.push_back(Edge(vertices[i].getCenterPointer(), vertices[j].getCenterPointer()));
 				}
 			}
@@ -415,6 +474,64 @@ public:
 	void pan(const HyperPoint& p, const HyperPoint& q) {
 		for (int i = 0; i < vertices.size(); i++) {
 			vertices[i].panCircle(p, q);
+		}
+	}
+	// Updating the position and velocity of the vertices according to dtime
+	void tick(long dtime) {
+		float deltaSlow = accel * (float)dtime / 1000.0f;
+		float percentageSlow = phi * (float)dtime / 1000.0f;
+		for (int i = 0; i < vertices.size(); i++) {
+			vec2 offset = velocity[i] * (float)dtime / 1000.0f;
+			if (!(length(velocity[i]) > deltaSlow)) {
+				velocity[i] = vec2(0, 0);
+			} else {
+				if (length(offset) > 0.001f) {
+					vertices[i].panCircle(HyperPoint(vec2(0, 0)), HyperPoint(offset));
+					velocity[i] = velocity[i] - velocity[i] * percentageSlow - normalize(velocity[i]) * deltaSlow;
+					//velocity[i] = velocity[i] - normalize(velocity[i]) * deltaSlow;
+				}
+			}
+			//vertices[i].updateBuf();
+		}
+	}
+	void heuristicIteration() {
+		for (int i = 0; i < velocity.size(); i++) {
+			velocity[i] = vec2(0, 0);
+		}
+		for (int i = 0; i < vertices.size(); i++) {
+			for (int j = 0; j < vertices.size(); j++) {
+				if (i == j || 
+					vertices[i].getCenter().position.x == vertices[j].getCenter().position.x || 
+					vertices[i].getCenter().position.y == vertices[j].getCenter().position.y /*||
+					!(abs(vertices[i].getCenter().position.x) < 1000.0f) ||
+					!(abs(vertices[i].getCenter().position.y) < 1000.0f) ||
+					!(abs(vertices[j].getCenter().position.x) < 1000.0f) ||
+					!(abs(vertices[j].getCenter().position.y) < 1000.0f) || 
+					!(abs(vertices[i].getCenter().position.x) > -1000.0f) ||
+					!(abs(vertices[i].getCenter().position.y) > -1000.0f) ||
+					!(abs(vertices[j].getCenter().position.x) > -1000.0f) ||
+					!(abs(vertices[j].getCenter().position.y) > -1000.0f)*/) {
+					// TODO: the void is watching you as well as the points at 0,0. WAKE UP WAKE UP WAKE UP
+				} else {
+					float distance = hyperDistance(vertices[j].getCenter(), vertices[i].getCenter());
+					if (std::find(vertices[i].center.pairs.begin(), vertices[i].center.pairs.end(), &vertices[j].center) != vertices[i].center.pairs.end()) {
+						float multiplier = (distance - desiredDistance);
+						vec3 dirVec = hyperDirectionVector(vertices[i].getCenter(), vertices[j].getCenter());
+						velocity[i] = velocity[i] - normalize(vec2(dirVec.x, dirVec.y)) * multiplier / 1.5f;
+						dirVec = hyperDirectionVector(vertices[j].getCenter(), vertices[i].getCenter());
+						velocity[j] = velocity[j] - normalize(vec2(dirVec.x, dirVec.y)) * multiplier / 1.5f;
+					} else {
+						float multiplier = sqrtf(fminf(desiredDistance / distance, desiredDistance));
+						vec3 dirVec = hyperDirectionVector(vertices[i].getCenter(), vertices[j].getCenter());
+						velocity[i] = velocity[i] + normalize(vec2(dirVec.x, dirVec.y)) * multiplier / 2;
+						dirVec = hyperDirectionVector(vertices[j].getCenter(), vertices[i].getCenter());
+						velocity[j] = velocity[j] + normalize(vec2(dirVec.x, dirVec.y)) * multiplier / 2;
+					}
+				}
+			}
+		}
+		for (int i = 0; i < vertices.size(); i++) {
+			vertices[i].iniCircle(vertices[i].tessellatedVertices);
 		}
 	}
 };
@@ -446,7 +563,11 @@ void onDisplay() {
 
 // Key of ASCII code pressed
 void onKeyboard(unsigned char key, int pX, int pY) {
-	if (key == 'd') glutPostRedisplay();         // if d, invalidate display, i.e. redraw
+	if (key == ' ') {
+		printf("\'%s\' was pressed!\n", &key);
+		graph.heuristicIteration();
+		glutPostRedisplay();
+	}
 }
 
 // Key of ASCII code released
@@ -499,7 +620,16 @@ void onMouse(int button, int state, int pX, int pY) { // pX, pY are the pixel co
 	}
 }
 
+// Time is previous segment, needed to calculate dtime.
+long oldTime = 0;
 // Idle event indicating that some time elapsed: do animation here
 void onIdle() {
-	long time = glutGet(GLUT_ELAPSED_TIME); // elapsed time since the start of the program
+	// Time in ms
+	long time = glutGet(GLUT_ELAPSED_TIME); // elapsed time since the start of the program, first step
+	long dtime = time - oldTime;
+	if (dtime < 100) {
+		graph.tick(dtime);
+	}
+	glutPostRedisplay(); // Redraw the scene
+	oldTime = time; // last step
 }
